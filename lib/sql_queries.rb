b603006df4c646686b9fdec0e7af0985ec33e58f
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-06-13 01:54:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color) AS MOST_FREQUENT
    FROM bears
    GROUP BY color
-   ORDER BY COUNT(color) DESC"
+   ORDER BY COUNT(color)"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
