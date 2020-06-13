2e44c4c19a086ced8f21e1fb53da86d24e084122
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-06-13 01:58:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,9 +20,9 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(color)
+  "SELECT color, COUNT(color) AS MOST_FREQUENT
    FROM bears
-   GROUP BY color AS MOST_FREQUENT"
+   GROUP BY color AS MOST_FREQUENT;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
