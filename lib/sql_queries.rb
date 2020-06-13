f88f36951d6669feca3d10969cae09ffafe0b3b9
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-06-13 02:01:55 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -22,7 +22,7 @@ end
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color)
    FROM bears WHERE color = (SELECT MAX(color) FROM bears);
-   GROUP BY color;"
+   ;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
